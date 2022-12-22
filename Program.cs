d8f551f7eabb468a1b097bc6fa600c154c7c75d4
seda <116501287+sedanurkorkmz1@users.noreply.github.com>
2022-12-22 23:51:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+  namespace operatorler
+ {
+     class Program
+     {
+         static void Main(string[] args)
+         {
+             
+
+               try
+               {
+                //int a = int.Parse(null);
+               int a = int.Parse("test");
+               }
+               catch (FormatException ex)
+               {
+                Console.WriteLine("veri tipi uygun değil.");
+                Console.WriteLine(ex);             
+              }
+              catch(OverflowException ex)
+              {
+              Console.WriteLine("çok küçük ya da çok büyük değer girdiniz");
+              Console.WriteLine(ex);
+              }
+              finally
+              {
+                Console.WriteLine("işlem başariyla tamamlandi");
+
+              }
+         }
+     }
+ }
